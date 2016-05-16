246896591ed0cf9939e4b3849e5c30b2d58830bc
Jeffrey D King <jdking33@aol.com>
2016-05-16 16:59:42 +0000

    fix indentation issues

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,27 +1,27 @@
 require_relative 'entry'
 
 class AddressBook
-   attr_reader :entries
+  attr_reader :entries
    
-   def initialize
-      @entries = []
-   end
+  def initialize
+    @entries = []
+  end
    
-   def add_entry(name, phone_number, email)
-      index = 0
-      entries.each do |entry|
-         if name < entry.name
-            break
-         end
-         index += 1
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
       end
-      entries.insert(index, Entry.new(name, phone_number, email))
-   end
+      index += 1
+    end
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
    
-   def remove_entry(name, phone_number, email)
-      entries.each_with_index do |entry, index|
-      if entries[index].name == name 
-         entries.delete_at(index)
-      end
-   end
+  def remove_entry(name, phone_number, email)
+    entries.each_with_index do |entry, index|
+    if entries[index].name == name 
+        entries.delete_at(index)
+    end
+  end
 end
